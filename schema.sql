84fddbaed6ad41adfb4453e958305ba6b0ed8a0f
Mathieu Lefebvre <mathieu@eosnation.io>
2023-11-21 10:14:55 -0500

    change sql schema

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS balance_changes  (
     contract FixedString(40),
-    owner Nullable(String),
-    old_balance Nullable(String),
-    new_balance Nullable(String),
-    transaction_id Nullable(String),
+    owner String,
+    old_balance String,
+    new_balance String,
+    transaction_id String,
 )
 ENGINE = MergeTree()
-ORDER BY (contract)
+ORDER BY (transaction_id)
