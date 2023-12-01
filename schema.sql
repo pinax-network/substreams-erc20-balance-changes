91bf98bb8d796d025f04d92ef73f769df2f8e716
Denis <denis@pinax.network>
2023-12-01 00:17:48 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,31 +1,32 @@
 -- Table for balance changes --
-CREATE TABLE IF NOT EXISTS balance_changes  (
+CREATE TABLE IF NOT EXISTS BalanceChange  (
     chain           LowCardinality(String),
     block_number    UInt32(),
     timestamp       DateTime64(3, 'UTC'),
     contract        FixedString(40),
     owner           FixedString(40),
-    old_balance     UInt256,
-    new_balance     UInt256,
-    transaction_id  FixedString(64),
+    transferValue   UInt256,
+    oldBalance      UInt256,
+    newBalance      UInt256,
+    transaction     FixedString(64),
 )
 ENGINE = MergeTree()
 ORDER BY (timestamp, block_number, chain);
 
 -- Indexes for block_number and chain --
-ALTER TABLE balance_changes ADD INDEX balance_changes_block_number_index block_number TYPE minmax;
-ALTER TABLE balance_changes ADD INDEX balance_changes_chain_index chain TYPE minmax;
+ALTER TABLE BalanceChange ADD INDEX balance_changes_block_number_index block_number TYPE minmax;
+ALTER TABLE BalanceChange ADD INDEX balance_changes_chain_index chain TYPE minmax;
 
 -- MV for contract --
 CREATE MATERIALIZED VIEW mv_balance_changes_contract
 ENGINE = MergeTree()
 ORDER BY (contract, owner)
 POPULATE
-AS SELECT * FROM balance_changes;
+AS SELECT * FROM BalanceChange;
 
 -- MV for owner --
 CREATE MATERIALIZED VIEW mv_balance_changes_owner
 ENGINE = MergeTree()
 ORDER BY (owner, contract)
 POPULATE
-AS SELECT * FROM balance_changes;
+AS SELECT * FROM BalanceChange;
