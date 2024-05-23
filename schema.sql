bfe0368e48b6a24c5a61fa18e8a43227c3a5b7e4
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2024-05-23 12:09:38 -0400

    fix

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 -- Table for balance changes --
-CREATE TABLE IF NOT EXISTS BalanceChange  (
+CREATE TABLE IF NOT EXISTS balance_changes  (
+    "id"            String,
     chain           LowCardinality(String),
     block_number    UInt32(),
     timestamp       DateTime64(3, 'UTC'),
@@ -10,23 +11,23 @@ CREATE TABLE IF NOT EXISTS BalanceChange  (
     newBalance      UInt256,
     transaction     FixedString(64),
 )
-ENGINE = MergeTree()
+ENGINE = MergeTree PRIMARY KEY ("id")
 ORDER BY (timestamp, block_number, chain);
 
 -- Indexes for block_number and chain --
-ALTER TABLE BalanceChange ADD INDEX balance_changes_block_number_index block_number TYPE minmax;
-ALTER TABLE BalanceChange ADD INDEX balance_changes_chain_index chain TYPE minmax;
+ALTER TABLE balance_changes ADD INDEX balance_changes_block_number_index block_number TYPE minmax;
+ALTER TABLE balance_changes ADD INDEX balance_changes_chain_index chain TYPE minmax;
 
 -- MV for contract --
 CREATE MATERIALIZED VIEW mv_balance_changes_contract
 ENGINE = MergeTree()
 ORDER BY (contract, owner)
 POPULATE
-AS SELECT * FROM BalanceChange;
+AS SELECT * FROM balance_changes;
 
 -- MV for owner --
 CREATE MATERIALIZED VIEW mv_balance_changes_owner
 ENGINE = MergeTree()
 ORDER BY (owner, contract)
 POPULATE
-AS SELECT * FROM BalanceChange;
+AS SELECT * FROM balance_changes;
