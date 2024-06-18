0fc52949003e74e7c947823dd604b164b9d60f24
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2024-06-18 13:17:02 -0400

    update

--- schema.sql
+++ schema.sql
@@ -1,76 +1,32 @@
+-- Table for balance changes --
 CREATE TABLE IF NOT EXISTS balance_changes  (
     "id"            String,
+    block_num    UInt32(),
     timestamp       DateTime64(3, 'UTC'),
     contract        FixedString(40),
-    account         FixedString(40),
+    owner           FixedString(40),
     amount          UInt256,
     old_balance     UInt256,
     new_balance     UInt256,
     transaction_id  FixedString(64),
-    block_num    UInt32(),
     change_type     Int32
 )
 ENGINE = MergeTree PRIMARY KEY ("id")
 ORDER BY (id,timestamp, block_num);
 
--- MV for contract --s
-CREATE MATERIALIZED VIEW balance_changes_contract_historical_mv
+-- Indexes for block_number --
+ALTER TABLE balance_changes ADD INDEX balance_changes_block_number_index block_num TYPE minmax;
+
+-- MV for contract --
+CREATE MATERIALIZED VIEW mv_balance_changes_contract
 ENGINE = MergeTree()
-ORDER BY (contract, account)
+ORDER BY (contract, owner)
 POPULATE
 AS SELECT * FROM balance_changes;
 
 -- MV for owner --
-CREATE MATERIALIZED VIEW balance_changes_account_historical_mv
+CREATE MATERIALIZED VIEW mv_balance_changes_owner
 ENGINE = MergeTree()
-ORDER BY (account, contract)
+ORDER BY (owner, contract)
 POPULATE
 AS SELECT * FROM balance_changes;
-
-
-CREATE TABLE IF NOT EXISTS token_holders
-(
-    account              String,
-    contract             String,
-    amount               Int64,
-    updated_at_block_num UInt64,
-    updated_at_timestamp DateTime,
-    is_deleted           UInt8
-)
-    ENGINE = ReplicatedReplacingMergeTree(block_num, is_deleted)
-        PRIMARY KEY (contract,account)
-        ORDER BY (contract, account);
-
-CREATE MATERIALIZED VIEW token_holders_mv
-    TO token_holders
-AS
-SELECT account,
-       contract,
-       amount,
-       block_num            AS updated_at_block_num,
-       timestamp            AS updated_at_timestamp,
-       if(amount > 0, 0, 1) AS is_deleted
-FROM balance_changes;
-
-CREATE TABLE IF NOT EXISTS account_balances
-(
-    account              String,
-    contract             String,
-    amount               Int64,
-    block_num            UInt32,
-    is_deleted           UInt8
-)
-    ENGINE = ReplicatedReplacingMergeTree(block_num, is_deleted)
-        PRIMARY KEY (account,contract)
-        ORDER BY (account,contract);
-
-CREATE MATERIALIZED VIEW account_balances_mv
-    TO account_balances
-AS
-SELECT account,
-       contract,
-       amount,
-       block_num            AS updated_at_block_num,
-       timestamp            AS updated_at_timestamp,
-       if(amount > 0, 0, 1) AS is_deleted
-FROM balance_changes;
